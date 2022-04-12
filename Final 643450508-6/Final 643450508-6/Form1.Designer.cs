410fff31c2dd47cb1681e9eb451850f0543a12a0
ink01 <101373437+ink01@users.noreply.github.com>
2022-04-13 02:19:30 +0700

    final

---
+++ Final 643450508-6/Final 643450508-6/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Final_643450508_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.foodbox1 = new System.Windows.Forms.TextBox();
+            this.foodbox3 = new System.Windows.Forms.TextBox();
+            this.foodbox5 = new System.Windows.Forms.TextBox();
+            this.foodbox2 = new System.Windows.Forms.TextBox();
+            this.foodbox4 = new System.Windows.Forms.TextBox();
+            this.foodbox6 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pricebox1 = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(483, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ink ตามสั่ง";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(442, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "เลือกรายการอาหาร";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(102, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "กะเพราไก่ (35 บาท)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.Location = new System.Drawing.Point(102, 283);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(202, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "กะเพราหมูกรอบ (50 บาท)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.Location = new System.Drawing.Point(110, 364);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "เพื่มไข่ดาว (5 บาท)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label6.Location = new System.Drawing.Point(612, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(184, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "กะเพราหมูสับ (40 บาท)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label7.Location = new System.Drawing.Point(612, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(168, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "กะเพราเนื้อ (40 บาท)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label8.Location = new System.Drawing.Point(612, 376);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(168, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "เพิ่มไข่เจียว (10 บาท)";
+            // 
+            // foodbox1
+            // 
+            this.foodbox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.foodbox1.Location = new System.Drawing.Point(367, 210);
+            this.foodbox1.Multiline = true;
+            this.foodbox1.Name = "foodbox1";
+            this.foodbox1.Size = new System.Drawing.Size(89, 37);
+            this.foodbox1.TabIndex = 8;
+            this.foodbox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // foodbox3
+            // 
+            this.foodbox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.foodbox3.Location = new System.Drawing.Point(367, 283);
+            this.foodbox3.Multiline = true;
+            this.foodbox3.Name = "foodbox3";
+            this.foodbox3.Size = new System.Drawing.Size(89, 37);
+            this.foodbox3.TabIndex = 9;
+            // 
+            // foodbox5
+            // 
+            this.foodbox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.foodbox5.Location = new System.Drawing.Point(367, 352);
+            this.foodbox5.Multiline = true;
+            this.foodbox5.Name = "foodbox5";
+            this.foodbox5.Size = new System.Drawing.Size(89, 37);
+            this.foodbox5.TabIndex = 10;
+            // 
+            // foodbox2
+            // 
+            this.foodbox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.foodbox2.Location = new System.Drawing.Point(827, 210);
+            this.foodbox2.Multiline = true;
+            this.foodbox2.Name = "foodbox2";
+            this.foodbox2.Size = new System.Drawing.Size(89, 37);
+            this.foodbox2.TabIndex = 11;
+            // 
+            // foodbox4
+            // 
+            this.foodbox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.foodbox4.Location = new System.Drawing.Point(827, 283);
+            this.foodbox4.Multiline = true;
+            this.foodbox4.Name = "foodbox4";
+            this.foodbox4.Size = new System.Drawing.Size(89, 37);
+            this.foodbox4.TabIndex = 12;
+            // 
+            // foodbox6
+            // 
+            this.foodbox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.foodbox6.Location = new System.Drawing.Point(827, 364);
+            this.foodbox6.Multiline = true;
+            this.foodbox6.Name = "foodbox6";
+            this.foodbox6.Size = new System.Drawing.Size(89, 37);
+            this.foodbox6.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label9.Location = new System.Drawing.Point(835, 181);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 20);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "จำนวนจาน";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label10.Location = new System.Drawing.Point(375, 181);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 20);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "จำนวนจาน";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(429, 443);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 63);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "คิดราคาอาหาร";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label11.Location = new System.Drawing.Point(195, 575);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(154, 25);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "จำนวนเงินที่ต้องจ่าย";
+            // 
+            // pricebox1
+            // 
+            this.pricebox1.Location = new System.Drawing.Point(429, 560);
+            this.pricebox1.Multiline = true;
+            this.pricebox1.Name = "pricebox1";
+            this.pricebox1.Size = new System.Drawing.Size(178, 40);
+            this.pricebox1.TabIndex = 18;
+            this.pricebox1.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label13.Location = new System.Drawing.Point(669, 575);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(43, 25);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "บาท";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 763);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.pricebox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.foodbox6);
+            this.Controls.Add(this.foodbox4);
+            this.Controls.Add(this.foodbox2);
+            this.Controls.Add(this.foodbox5);
+            this.Controls.Add(this.foodbox3);
+            this.Controls.Add(this.foodbox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox foodbox1;
+        private System.Windows.Forms.TextBox foodbox3;
+        private System.Windows.Forms.TextBox foodbox5;
+        private System.Windows.Forms.TextBox foodbox2;
+        private System.Windows.Forms.TextBox foodbox4;
+        private System.Windows.Forms.TextBox foodbox6;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox pricebox1;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Final 643450508-6/Final 643450508-6/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_643450508_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+            if (foodbox1.Text!= "")
+        
+               total +=Convert.ToInt32(foodbox1.Text) * 35;
+
+            if (foodbox2.Text != "")
+
+                total += Convert.ToInt32(foodbox2.Text) * 40;
+
+            if (foodbox3.Text != "")
+
+                total += Convert.ToInt32(foodbox3.Text) * 50;
+
+            if (foodbox4.Text != "")
+
+                total += Convert.ToInt32(foodbox4.Text) * 40;
+
+            if (foodbox5.Text != "")
+
+                total += Convert.ToInt32(foodbox5.Text) * 5;
+
+            if (foodbox6.Text != "")
+
+                total += Convert.ToInt32(foodbox6.Text) * 10;
+
+            pricebox1.Text = total.ToString();
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
